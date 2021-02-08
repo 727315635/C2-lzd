4529cb45cdf9c8e96d384834ae9324587b931e1f
lzd <727315635@qq.com>
2021-02-08 17:02:03 +0800

    第五道有点行

---
+++ week2/src/Task5/Monkey.java
@@ -0,0 +1,12 @@
+package Task5;
+
+public class Monkey {
+
+
+    public void speak()
+    {
+        System.out.println("咿咿呀呀......");
+
+    }
+
+}
---
+++ week2/src/Task5/main.java
@@ -0,0 +1,12 @@
+package Task5;
+
+public class main {
+    public static void main(String[] args) {
+        Monkey monkey=new Monkey();
+        People people = new People();
+        monkey.speak();
+        people.speak();
+        people.think();
+
+    }
+}
